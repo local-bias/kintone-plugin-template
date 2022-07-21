288a61b0477f619f2d43556a6a3822c12a090e51
Ribbit <echo07200503@gmail.com>
2022-07-21 13:19:11 +0900

    👍 React18対応

--- src/config/main.tsx
+++ src/config/main.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
-import { render } from 'react-dom';
+import { createRoot } from 'react-dom/client';
 
 import App from './app';
 
 const main = (pluginId: string): void => {
-  render(<App {...{ pluginId }} />, document.getElementById('settings'));
+  const root = document.getElementById('settings');
+  if (!root) {
+    throw 'プラグインのHTMLに、ルート要素が存在しません。';
+  }
+  createRoot(root).render(<App {...{ pluginId }} />);
 };
 
 export default main;
