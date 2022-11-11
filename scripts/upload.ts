b43097358ea86559697a5500dfde9af5e336d2e1
Ribbit <echo07200503@gmail.com>
2022-11-11 11:00:32 +0900

    👍 .envの変数名をrest-apiに準拠

--- scripts/upload.ts
+++
@@ -1,9 +0,0 @@
-const { exec } = require('child_process');
-
-require('dotenv').config();
-
-(() => {
-  exec(
-    `kintone-plugin-uploader dist/plugin-dev.zip --base-url ${process.env.KINTONE_URL} --username ${process.env.LOGIN_NAME} --password ${process.env.LOGIN_PASS} --watch --waiting-dialog-ms 3000`
-  );
-})();
