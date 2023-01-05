7592c8f521a746d429415c2eb823f2dfa7cde3fc
Ribbit <echo07200503@gmail.com>
2023-01-05 14:27:57 +0900

    📝 バージョン情報をパッケージと統一

--- plugin.config.js
+++ plugin.config.js
@@ -3,7 +3,7 @@ module.exports = {
   manifest: {
     base: {
       manifest_version: 1,
-      version: 1,
+      version: '1.0.0',
       type: 'APP',
       name: {
         en: 'kintone-plugin-template',
