c996c6c869fe37923ca1cd65f4a714ea99612413
Ribbit <echo07200503@gmail.com>
2023-06-24 22:13:10 +0900

    🔥 未使用の型定義を削除

--- src/types/react.d.ts
+++ src/types/react.d.ts
@@ -3,9 +3,4 @@ import React from 'react';
 declare module 'react' {
   /** classNameを追加したファンクションコンポーネント */
   type FCX<P = Record<string, unknown>> = React.FunctionComponent<P & { className?: string }>;
-
-  /** childrenを追加したファンクションコンポーネント */
-  type FCwC<P = Record<string, unknown>> = React.FunctionComponent<
-    P & { children: React.ReactNode }
-  >;
 }
