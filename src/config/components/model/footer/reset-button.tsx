5d7e0a6c44990f7834ee2d737b87fc964d25bd43
Ribbit <echo07200503@gmail.com>
2023-11-10 15:29:43 +0900

    ♻ Refactor reset button component to use PluginConfigResetButton from @konomi-app/kintone-utilities-react.

--- src/config/components/model/footer/reset-button.tsx
+++ src/config/components/model/footer/reset-button.tsx
@@ -1,67 +1,23 @@
-import React, { FC, memo, useCallback, useState } from 'react';
-import {
-  Button,
-  Dialog,
-  DialogActions,
-  DialogContent,
-  DialogContentText,
-  DialogTitle,
-  IconButton,
-  Tooltip,
-} from '@mui/material';
-import RestartAltIcon from '@mui/icons-material/RestartAlt';
+import React, { FC, memo } from 'react';
 import { useRecoilCallback } from 'recoil';
 import { useSnackbar } from 'notistack';
 import { storageState } from '../../../states/plugin';
 import { createConfig } from '@/lib/plugin';
+import { PluginConfigResetButton } from '@konomi-app/kintone-utilities-react';
 
 const Component: FC = () => {
   const { enqueueSnackbar } = useSnackbar();
-  const [open, setOpen] = useState<boolean>(false);
 
-  const onIconButtonClick = useCallback(() => {
-    setOpen(true);
-  }, []);
-
-  const onDecisionButtonClick = useRecoilCallback(
+  const reset = useRecoilCallback(
     ({ set }) =>
       () => {
         set(storageState, createConfig());
-        setOpen(false);
         enqueueSnackbar('設定をリセットしました', { variant: 'success' });
       },
     []
   );
 
-  const onClose = useCallback(() => {
-    setOpen(false);
-  }, []);
-
-  return (
-    <>
-      <Dialog open={open} onClose={onClose}>
-        <DialogTitle>設定のリセット</DialogTitle>
-        <DialogContent>
-          <DialogContentText>
-            このプラグインの設定を初期状態に戻します。よろしいですか？
-          </DialogContentText>
-        </DialogContent>
-        <DialogActions>
-          <Button color='primary' variant='contained' onClick={onDecisionButtonClick}>
-            実行
-          </Button>
-          <Button color='inherit' variant='contained' onClick={onClose}>
-            キャンセル
-          </Button>
-        </DialogActions>
-      </Dialog>
-      <Tooltip title='プラグインの設定をリセット'>
-        <IconButton onClick={onIconButtonClick}>
-          <RestartAltIcon />
-        </IconButton>
-      </Tooltip>
-    </>
-  );
+  return <PluginConfigResetButton reset={reset} />;
 };
 
 export default memo(Component);
