4e029e0a9959c1cb6865aaf4a7ff16a05d052797
Ribbit <echo07200503@gmail.com>
2023-11-25 14:30:11 +0900

    ♻ Refactor app configuration and add plugin notifications

--- src/config/app.tsx
+++ src/config/app.tsx
@@ -1,32 +1,40 @@
-import { PluginBanner, PluginContent, PluginLayout } from '@konomi-app/kintone-utilities-react';
+import {
+  PluginBanner,
+  PluginContent,
+  PluginLayout,
+  PluginConfigProvider,
+  Notification,
+} from '@konomi-app/kintone-utilities-react';
 import { LoaderWithLabel } from '@konomi-app/ui-react';
 import { SnackbarProvider } from 'notistack';
 import React, { FC, Suspense } from 'react';
 import { RecoilRoot } from 'recoil';
 import { URL_BANNER, URL_PROMOTION } from '@/lib/static';
 import { PluginErrorBoundary } from '@/lib/components/error-boundary';
-import Announcement from './components/model/announcement';
 import Footer from './components/model/footer';
 import Form from './components/model/form';
 import Sidebar from './components/model/sidebar';
+import config from '../../plugin.config.mjs';
 
 const Component: FC = () => (
   <Suspense fallback={<LoaderWithLabel label='画面の描画を待機しています' />}>
     <RecoilRoot>
       <PluginErrorBoundary>
-        <Announcement />
-        <SnackbarProvider maxSnack={1}>
-          <Suspense fallback={<LoaderWithLabel label='設定情報を取得しています' />}>
-            <PluginLayout>
-              <Sidebar />
-              <PluginContent>
-                <Form />
-              </PluginContent>
-              <PluginBanner url={URL_BANNER} />
-              <Footer />
-            </PluginLayout>
-          </Suspense>
-        </SnackbarProvider>
+        <PluginConfigProvider config={config}>
+          <Notification />
+          <SnackbarProvider maxSnack={1}>
+            <Suspense fallback={<LoaderWithLabel label='設定情報を取得しています' />}>
+              <PluginLayout>
+                <Sidebar />
+                <PluginContent>
+                  <Form />
+                </PluginContent>
+                <PluginBanner url={URL_BANNER} />
+                <Footer />
+              </PluginLayout>
+            </Suspense>
+          </SnackbarProvider>
+        </PluginConfigProvider>
       </PluginErrorBoundary>
     </RecoilRoot>
     <iframe title='promotion' loading='lazy' src={URL_PROMOTION} className='border-0 w-full h-16' />
