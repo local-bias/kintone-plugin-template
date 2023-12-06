5de32e55576963ab293023920a5cc121793c8a6a
Ribbit <echo07200503@gmail.com>
2023-12-06 14:51:57 +0900

    🔥 Remove unused variables and imports

--- src/lib/static.ts
+++ src/lib/static.ts
@@ -1,10 +1,6 @@
 import config from '../../plugin.config.mjs';
 
 export const PLUGIN_NAME = config.manifest.base.name.ja;
-export const PLUGIN_VERSION = config.manifest.base.version;
-export const LOCAL_STORAGE_KEY = config.id;
-export const URL_HOMEPAGE = config.manifest.base.homepage_url?.ja;
-export const URL_PLUGIN_LIST = config.pluginReleasePageUrl;
 
 export const URL_INQUIRY = 'https://form.konomi.app';
 export const URL_PROMOTION = 'https://promotion.konomi.app/kintone-plugin';
