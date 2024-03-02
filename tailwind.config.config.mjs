bc77d8cde08047271477ac956fa0fee88efcf042
Ribbit <echo07200503@gmail.com>
2024-03-02 10:45:36 +0900

    ✨ shadcnui

--- tailwind.config.config.mjs
+++ tailwind.config.config.mjs
@@ -4,5 +4,9 @@ import common from './tailwind.config.common.mjs';
 /** @type { import('tailwindcss').Config } */
 export default {
   ...common,
-  content: ['./src/lib/**/*.{ts,js,jsx,tsx}', './src/config/**/*.{ts,js,jsx,tsx}'],
+  content: [
+    './src/components/**/*.{ts,tsx}',
+    './src/lib/**/*.{ts,tsx}',
+    './src/config/**/*.{ts,tsx}',
+  ],
 };
