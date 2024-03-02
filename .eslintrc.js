9bdcfc6991ffc92b94d93bbab47421811b40629a
Ribbit <echo07200503@gmail.com>
2024-03-02 10:44:57 +0900

    🔥 依存関係が解決するまでairbを削除

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,7 +3,7 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: ['plugin:react/recommended', 'airbnb', 'airbnb-typescript', 'prettier'],
+  extends: ['plugin:react/recommended', 'prettier'],
   overrides: [
     {
       env: {
