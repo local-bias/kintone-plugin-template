5b50910e8e52921b5c4a32cc7a8ed3986397231e
Ribbit <echo07200503@gmail.com>
2024-04-05 11:13:12 +0900

    ✨ localize mui

--- src/config/app.tsx
+++ src/config/app.tsx
@@ -15,32 +15,50 @@ import config from 'plugin.config.mjs';
 import Footer from './components/model/footer';
 import Form from './components/model/form';
 import Sidebar from './components/model/sidebar';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { jaJP, enUS, zhCN, esES } from '@mui/material/locale';
+import { LANGUAGE } from '@/lib/global';
 
-const Component: FC = () => (
+const Component: FC = () => {
+  return (
+    <>
+      <Sidebar />
+      <PluginContent>
+        <PluginErrorBoundary>
+          <Form />
+        </PluginErrorBoundary>
+      </PluginContent>
+      <PluginBanner url={URL_BANNER} />
+      <Footer />
+    </>
+  );
+};
+
+const Container: FC = () => (
   <Suspense fallback={<LoaderWithLabel label='画面の描画を待機しています' />}>
-    <RecoilRoot>
-      <PluginErrorBoundary>
-        <PluginConfigProvider config={config}>
-          <Notification />
-          <SnackbarProvider maxSnack={1}>
-            <Suspense fallback={<LoaderWithLabel label='設定情報を取得しています' />}>
-              <PluginLayout>
-                <Sidebar />
-                <PluginContent>
-                  <PluginErrorBoundary>
-                    <Form />
-                  </PluginErrorBoundary>
-                </PluginContent>
-                <PluginBanner url={URL_BANNER} />
-                <Footer />
-              </PluginLayout>
-            </Suspense>
-          </SnackbarProvider>
-        </PluginConfigProvider>
-      </PluginErrorBoundary>
-    </RecoilRoot>
+    <ThemeProvider
+      theme={createTheme(
+        {},
+        LANGUAGE === 'en' ? enUS : LANGUAGE === 'zh' ? zhCN : LANGUAGE === 'es' ? esES : jaJP
+      )}
+    >
+      <RecoilRoot>
+        <PluginErrorBoundary>
+          <PluginConfigProvider config={config}>
+            <Notification />
+            <SnackbarProvider maxSnack={1}>
+              <Suspense fallback={<LoaderWithLabel label='設定情報を取得しています' />}>
+                <PluginLayout>
+                  <Component />
+                </PluginLayout>
+              </Suspense>
+            </SnackbarProvider>
+          </PluginConfigProvider>
+        </PluginErrorBoundary>
+      </RecoilRoot>
+    </ThemeProvider>
     <iframe title='promotion' loading='lazy' src={URL_PROMOTION} className='border-0 w-full h-16' />
   </Suspense>
 );
 
-export default Component;
+export default Container;
--- src/lib/global.ts
+++ src/lib/global.ts
@@ -2,7 +2,7 @@ import { detectGuestSpaceId } from '@konomi-app/kintone-utilities';
 
 export const PLUGIN_ID = kintone.$PLUGIN_ID;
 export const GUEST_SPACE_ID = detectGuestSpaceId() ?? undefined;
-export const LANGUAGE = kintone.getLoginUser().language;
+export const LANGUAGE = kintone.getLoginUser().language as 'ja' | 'en' | 'zh' | 'es';
 
 process.env.NODE_ENV === 'development' &&
   console.log('[plugin] Global variables have been redefined');
