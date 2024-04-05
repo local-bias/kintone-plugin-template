5be671b601d75eebf4aba87cfe2dbb55cd046b95
Ribbit <echo07200503@gmail.com>
2024-04-05 10:55:45 +0900

    👍 エラー発生時の影響範囲を制限

--- src/config/app.tsx
+++ src/config/app.tsx
@@ -27,7 +27,9 @@ const Component: FC = () => (
               <PluginLayout>
                 <Sidebar />
                 <PluginContent>
-                  <Form />
+                  <PluginErrorBoundary>
+                    <Form />
+                  </PluginErrorBoundary>
                 </PluginContent>
                 <PluginBanner url={URL_BANNER} />
                 <Footer />
