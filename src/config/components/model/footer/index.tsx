2bec33bf8a8358a20d0568fa8cac9d89ceb7f045
Ribbit <echo07200503@gmail.com>
2024-05-15 14:15:04 +0900

    ♻ プラグインのインポート・エクスポート処理をカスタムフックにまとめる

--- src/config/components/model/footer/index.tsx
+++ src/config/components/model/footer/index.tsx
@@ -1,18 +1,19 @@
+import { usePluginStorage } from '@/config/hooks/use-plugin-storage';
+import { t } from '@/lib/i18n';
 import { storeStorage } from '@konomi-app/kintone-utilities';
+import {
+  PluginConfigExportButton,
+  PluginConfigImportButton,
+  PluginFooter,
+} from '@konomi-app/kintone-utilities-react';
 import SaveIcon from '@mui/icons-material/Save';
 import SettingsBackupRestoreIcon from '@mui/icons-material/SettingsBackupRestore';
 import { Button, CircularProgress } from '@mui/material';
 import { useSnackbar } from 'notistack';
 import React, { FC, useCallback } from 'react';
 import { useRecoilCallback, useRecoilValue } from 'recoil';
-
-import { PluginFooter } from '@konomi-app/kintone-utilities-react';
 import { loadingState, storageState } from '../../../states/plugin';
-
-import ExportButton from './export-button';
-import ImportButton from './import-button';
 import ResetButton from './reset-button';
-import { t } from '@/lib/i18n';
 
 type Props = {
   onSaveButtonClick: () => void;
@@ -21,6 +22,7 @@ type Props = {
 
 const Component: FC<Props> = ({ onSaveButtonClick, onBackButtonClick }) => {
   const loading = useRecoilValue(loadingState);
+  const { exportStorage, importStorage } = usePluginStorage();
 
   return (
     <PluginFooter className='py-2'>
@@ -47,8 +49,8 @@ const Component: FC<Props> = ({ onSaveButtonClick, onBackButtonClick }) => {
         </Button>
       </div>
       <div className='flex items-center gap-4'>
-        <ExportButton />
-        <ImportButton />
+        <PluginConfigExportButton loading={loading} onExportButtonClick={exportStorage} />
+        <PluginConfigImportButton onImportButtonClick={importStorage} loading={loading} />
         <ResetButton />
       </div>
     </PluginFooter>
---
+++ src/config/hooks/use-plugin-storage.ts
@@ -0,0 +1,62 @@
+import { useRecoilCallback } from 'recoil';
+import { loadingState, storageState } from '../states/plugin';
+import { PLUGIN_NAME } from '@/lib/static';
+import { useSnackbar } from 'notistack';
+import { t } from '@/lib/i18n';
+import { ChangeEventHandler } from 'react';
+import { onFileLoad } from '@konomi-app/kintone-utilities';
+import { migrateConfig } from '@/lib/plugin';
+
+export const usePluginStorage = () => {
+  const { enqueueSnackbar } = useSnackbar();
+
+  const importStorage: ChangeEventHandler<HTMLInputElement> = useRecoilCallback(
+    ({ set }) =>
+      async (event) => {
+        try {
+          const { files } = event.target;
+          if (!files?.length) {
+            return;
+          }
+          const [file] = Array.from(files);
+          const fileEvent = await onFileLoad(file);
+          const text = (fileEvent.target?.result ?? '') as string;
+          set(storageState, migrateConfig(JSON.parse(text)));
+          enqueueSnackbar(t('config.toast.import'), { variant: 'success' });
+        } catch (error) {
+          enqueueSnackbar(t('config.error.import'), { variant: 'error' });
+          throw error;
+        }
+      },
+    []
+  );
+
+  const exportStorage = useRecoilCallback(
+    ({ set, snapshot }) =>
+      async () => {
+        try {
+          set(loadingState, true);
+          const storage = await snapshot.getPromise(storageState);
+          const blob = new Blob([JSON.stringify(storage, null)], {
+            type: 'application/json',
+          });
+          const url = (window.URL || window.webkitURL).createObjectURL(blob);
+          const link = document.createElement('a');
+          link.download = `${PLUGIN_NAME}-config.json`;
+          link.href = url;
+          document.body.appendChild(link);
+          link.click();
+          document.body.removeChild(link);
+          enqueueSnackbar(t('config.toast.export'), { variant: 'success' });
+        } catch (error) {
+          enqueueSnackbar(t('config.error.export'), { variant: 'error' });
+          throw error;
+        } finally {
+          set(loadingState, false);
+        }
+      },
+    []
+  );
+
+  return { importStorage, exportStorage };
+};
