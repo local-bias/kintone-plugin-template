675e89606f6929f42e44c4729eabc5e5b270f85f
Ribbit <echo07200503@gmail.com>
2024-07-22 17:11:29 +0900

    👮 プラグインのスタイルの適用範囲を制限

--- tailwind.config.desktop.mjs
+++ tailwind.config.desktop.mjs
@@ -4,6 +4,7 @@ import common from './tailwind.config.common.mjs';
 /** @type { import('tailwindcss').Config } */
 export default {
   ...common,
+  important: '.🐸',
   content: [
     './src/components/**/*.{ts,tsx}',
     './src/lib/**/*.{ts,tsx}',
