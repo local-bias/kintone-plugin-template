686b5eeb857c99d262ae7dd1cc081d1485d49baa
Ribbit <echo07200503@gmail.com>
2024-07-27 22:13:23 +0900

    👍 初期バージョンを設定

--- plugin.config.mjs
+++ plugin.config.mjs
@@ -23,7 +23,7 @@ export default /** @type { const } */ ({
   manifest: {
     base: {
       manifest_version: 1,
-      version: '2.0.1',
+      version: '0.1.0',
       type: 'APP',
       name: {
         en: 'kintone-plugin-template',
