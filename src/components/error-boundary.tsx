b09e3873e3fd77437b799034330191c054c59783
Ribbit <echo07200503@gmail.com>
2024-11-20 18:11:59 +0900

    🔀 merge

--- src/components/error-boundary.tsx
+++ src/components/error-boundary.tsx
@@ -6,7 +6,7 @@ import { LoaderWithLabel } from '@konomi-app/ui-react';
 import styled from '@emotion/styled';
 import config from 'plugin.config.mjs';
 
-const Component: FC<FallbackProps & { className?: string }> = ({
+const ErrorFallbackComponent: FC<FallbackProps & { className?: string }> = ({
   className,
   error,
   resetErrorBoundary,
@@ -99,7 +99,7 @@ const Component: FC<FallbackProps & { className?: string }> = ({
   );
 };
 
-const StyledComponent = styled(Component)`
+const StyledErrorFallback = styled(ErrorFallbackComponent)`
   margin: 8px;
 
   h2 {
@@ -137,8 +137,6 @@ const StyledComponent = styled(Component)`
   }
 `;
 
-const Container: FC<{ children: React.ReactNode }> = ({ children }) => (
-  <ErrorBoundary FallbackComponent={StyledComponent}>{children}</ErrorBoundary>
+export const PluginErrorBoundary: FC<{ children: React.ReactNode }> = ({ children }) => (
+  <ErrorBoundary FallbackComponent={StyledErrorFallback}>{children}</ErrorBoundary>
 );
-
-export const PluginErrorBoundary = Container;
--- src/desktop/event.tsx
+++ src/desktop/event.tsx
@@ -1,5 +1,7 @@
 import { ThemeProvider } from '@/components/theme-provider';
+import { ComponentManager } from '@/lib/component-manager';
 import { manager } from '@/lib/event-manager';
+import { isProd } from '@/lib/global';
 import { t } from '@/lib/i18n';
 import { restorePluginConfig } from '@/lib/plugin';
 import { PLUGIN_NAME } from '@/lib/static';
@@ -9,11 +11,8 @@ import { Provider } from 'jotai';
 import { Rocket } from 'lucide-react';
 import config from 'plugin.config.mjs';
 import React, { FC, useState } from 'react';
-import { Root, createRoot } from 'react-dom/client';
 
 const ROOT_ID = `🐸${config.id}-root`;
-
-let cachedRoot: Root | null = null;
 
 const Component: FC<{ pluginConfig: Plugin.Config }> = ({ pluginConfig }) => {
   const [open, setOpen] = useState(false);
@@ -51,23 +50,19 @@ const Component: FC<{ pluginConfig: Plugin.Config }> = ({ pluginConfig }) => {
 manager.add(['app.record.index.show', 'app.record.detail.show'], async (event) => {
   const config = restorePluginConfig();
 
-  if (!cachedRoot || !document.getElementById(ROOT_ID)) {
-    const rootElement = document.createElement('div');
-    rootElement.id = ROOT_ID;
-    document.body.append(rootElement);
+  const componentManager = ComponentManager.getInstance();
+  componentManager.debug = !isProd;
 
-    const root = createRoot(rootElement);
-
-    cachedRoot = root;
-  }
-
-  cachedRoot.render(
-    <Provider store={store}>
-      <ThemeProvider>
-        <Component pluginConfig={config} />
-      </ThemeProvider>
-    </Provider>
-  );
+  componentManager.renderComponent({
+    elementId: ROOT_ID,
+    component: (
+      <Provider store={store}>
+        <ThemeProvider>
+          <Component pluginConfig={config} />
+        </ThemeProvider>
+      </Provider>
+    ),
+  });
 
   return event;
 });
